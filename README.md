0a669f051872c7d6b058c3f806af658d88b868a7
karimoreira <kari.moreira@cs.cruzeirodosul.edu.br>
2025-07-19 19:33:03 -0300

    feat; inclusao das imgs dos testes

---
+++ README.md
@@ -0,0 +1,9 @@
+# simulação e análise de curvas de luz
+
+repositório para estudar e entender em fortran os dados observacionais. escolhi o fortran por ser uma linguagem conhecida na área e comecei pelo básico, com a intenção de expandir e voltar a estudar sobre a análise de curvas de luz porém em código.
+
+<div align="center">
+  <img src="img/msys2.png" width="45%" style="margin-right: 10px;">
+  <img src="img/transito.png"  width="45%">
+</div>
+
--- curva_luz.f90
+++ curva_luz.f90
@@ -2,6 +2,7 @@ program curva_luz
     implicit none
     integer :: n, i, ios
     real, allocatable :: tempo(:), magnitude(:), erro(:)
+    real, allocatable :: mag_sorted(:)
     real :: media, soma_pesos, media_ponderada, desvio
     real :: soma_t, soma_m, soma_tt, soma_tm, a, b
     character(len=100) :: filename
@@ -16,7 +17,7 @@ program curva_luz
     end do
     close(10)
 
-    allocate(tempo(n), magnitude(n), erro(n))
+    allocate(tempo(n), magnitude(n), erro(n), mag_sorted(n))
 
     open(unit=10, file=filename, status='old', action='read')
     do i = 1, n
@@ -41,5 +42,78 @@ program curva_luz
     a = (n * soma_tm - soma_t * soma_m) / (n * soma_tt - soma_t**2)
     b = (soma_m * soma_tt - soma_t * soma_tm) / (n * soma_tt - soma_t**2)
     print *, 'Ajuste linear: magnitude = ', a, '* tempo + ', b
+
+    print *, 'Valor minimo: ', minval(magnitude)
+    print *, 'Valor maximo: ', maxval(magnitude)
+
+    mag_sorted = magnitude
+    call sort_array(mag_sorted, n)
+    if (mod(n,2) == 0) then
+        print *, 'Mediana: ', (mag_sorted(n/2) + mag_sorted(n/2+1))/2.0
+    else
+        print *, 'Mediana: ', mag_sorted((n+1)/2)
+    end if
+
+    print *, 'Moda: ', moda(magnitude, n)
+
+    print *, 'Q1: ', quartil(mag_sorted, n, 1)
+    print *, 'Q2 (Mediana): ', quartil(mag_sorted, n, 2)
+    print *, 'Q3: ', quartil(mag_sorted, n, 3)
+
+    deallocate(tempo, magnitude, erro, mag_sorted)
+
+contains
+
+subroutine sort_array(arr, n)
+    real, intent(inout) :: arr(:)
+    integer, intent(in) :: n
+    integer :: i, j
+    real :: temp
+    do i = 1, n-1
+        do j = i+1, n
+            if (arr(i) > arr(j)) then
+                temp = arr(i)
+                arr(i) = arr(j)
+                arr(j) = temp
+            end if
+        end do
+    end do
+end subroutine sort_array
+
+function moda(arr, n) result(mode)
+    real, intent(in) :: arr(:)
+    integer, intent(in) :: n
+    real :: mode
+    integer :: i, j, count, max_count
+    mode = arr(1)
+    max_count = 1
+    do i = 1, n
+        count = 1
+        do j = i+1, n
+            if (arr(j) == arr(i)) count = count + 1
+        end do
+        if (count > max_count) then
+            max_count = count
+            mode = arr(i)
+        end if
+    end do
+end function moda
+
+function quartil(arr, n, q) result(qv)
+    real, intent(in) :: arr(:)
+    integer, intent(in) :: n, q
+    real :: qv
+    real :: pos
+    integer :: idx
+    pos = q * (n+1) / 4.0
+    idx = int(pos)
+    if (idx < 1) idx = 1
+    if (idx >= n) idx = n
+    if (pos == idx) then
+        qv = arr(idx)
+    else
+        qv = arr(idx) + (arr(idx+1) - arr(idx)) * (pos - idx)
+    end if
+end function quartil
 
 end program curva_luz
